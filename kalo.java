18950f097643b3f25354874a7769f8994c2ad8c0
Jack Bowe <97858762+r4yhdez@users.noreply.github.com>
2024-08-03 13:19:59 +0700

    main.java

---
+++ kalo.java
@@ -0,0 +1,29 @@
+ import java.util.Scanner;
+class sc {
+	public static void main(String args[]) {
+		// header input
+		Scanner userInput = new Scanner(System.in);
+		
+		//minta angka pertama
+		System.out.println("masukan angka pertama");
+		int a = userInput.nextInt();
+		
+		//minta angka ke 2
+		
+		System.out.println("masukan angka ke 2");
+		int b = userInput.nextInt();
+		
+		// artimatika pertambahan
+		
+		int result;
+		
+		result = a + b;
+		
+	   //menunjukan hasil
+	   System.out.println(result);
+		
+		
+		
+		
+	}
+}
